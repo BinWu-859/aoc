9f01efb2d194ef7e036b759675458ab10a8f6f6a
Bin Wu <binwu@axis.com>
2024-12-12 15:51:34 +0800

    2023Day23

--- 2023/23.py
+++ 2023/23.py
@@ -7,7 +7,7 @@ for line in sys.stdin:
 
 node_map = {
     '#':[],
-    '.':[(-1, 0), (0, 1), (1, 0), (0, -1)],
+    '.':[(1, 0), (-1, 0), (0, 1), (0, -1)],
     '>':[(0, 1)],
     '<':[(0, -1)],
     'v':[(1, 0)],
@@ -17,13 +17,12 @@ node_map = {
 sr, sc = 0, map[0].index('.')
 er, ec = len(map) - 1, map[-1].index('.')
 joint ={}
-def count_d(r, c):
-    count = 0
-    if map[r][c] == '#':
-        return 0
-    if r==er and c==ec:
-        return 4
-    for d in node_map['.']:
+
+def get_d(r, c, ld):
+    ret = []
+    for d in node_map[map[r][c]]:
+        if ld[0] + d[0] == 0 and ld[1] + d[1] == 0:
+            continue
         nr = r + d[0]
         nc = c + d[1]
         if 0 > nr:
@@ -31,60 +30,61 @@ def count_d(r, c):
         if nr >= len(map):
             continue
         if map[nr][nc] != '#':
-            count += 1
-    return count
+            ret.append(d)
+    return ret
 
 jc = 1
 joint[(sr, sc)] = 1
-for i in range(len(map)):
-    for j in range(len(map[0])):
-        if count_d(i, j) > 2:
-            joint[(i, j)] = 1 << jc
-            jc += 1
 
-def count(slope_enabled=True):
-    res = []
-
-    stack = [(sr, sc, 0, 0, (1, 0))]
+def build_graph():
+    global jc
+    graph = {}
+    stack = [i for i in joint.keys()]
     while stack:
         n = stack.pop()
+        ds = get_d(n[0], n[1], (0, 0))
         if n[0] == er and n[1] == ec:
-            res.append(n[2])
-            #print(n[2])
             continue
-        nr = n[0]
-        nc = n[1]
-        nv = n[2]
-        nn = n[3]
-        nn = nn | joint[nr, nc]
+        for d in ds:
+            c = 1
+            nr = n[0] + d[0]
+            nc = n[1] + d[1]
+            nds = get_d(nr, nc, d)
+            while len(nds) == 1:
+                nr += nds[0][0]
+                nc += nds[0][1]
+                c += 1
+                nds = get_d(nr, nc, nds[0])
+            if (nr, nc) not in joint:
+                stack.append((nr, nc))
+                joint[(nr, nc)] = 1 << jc
+                jc += 1
+            if joint[n] not in graph:
+                graph[joint[n]] = {}
+            if len(nds) > 0 or (nr == er and nc == ec):
+                graph[joint[n]][joint[(nr, nc)]] = c
+    return graph
 
-        for d in node_map[map[nr][nc]] if slope_enabled else [n[4], (n[4][1], -n[4][0]), (-n[4][1], n[4][0])]:
-            tr = nr + d[0]
-            tc = nc + d[1]
-            tv = nv + 1
-            ld = (d[0], d[1])
-            if map[tr][tc] == '#':
-                continue
-
-            while (tr, tc) not in joint:
-                for td in node_map[map[tr][tc]] if slope_enabled else [ld, (-ld[1], ld[0]), (ld[1], -ld[0])]:
-                    if map[tr + td[0]][tc + td[1]] == '#':
-                        continue
-                    tr = tr + td[0]
-                    tc = tc + td[1]
-                    tv += 1
-                    ld = td
-                    break
-
-            #(tr, tc) in joint:
-            if (joint[(tr, tc)] & nn) == 0:
-                stack.append((tr, tc, tv, nn, ld))
-            else:
-                continue
-
-    return max(res)
+def run():
+    g = build_graph()
+    stk = [(1, 0, 1)]
+    steps = []
+    while stk:
+        n,s,p = stk.pop()
+        if n == joint[(er, ec)]:
+            steps.append(s)
+            continue
+        for l in g[n]:
+            if l & p == 0:
+                stk.append((l, s+g[n][l], l|p))
+    return(max(steps))
+print(run())
 
 
-print(count(True), count(False))
+for i, l in enumerate(map):
+    for c in '<>^v':
+        l = l.replace(c, '.')
+    map[i] = l
+print(run())
 
 
